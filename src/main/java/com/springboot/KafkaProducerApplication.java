5b27c3574a0bd282238bba1c35280f8ceb5b4019
Ranjeet Singh <RanjeetSi@hcl.com>
2020-02-10 15:28:14 +0530

    commit kofka producer

---
+++ src/main/java/com/springboot/KafkaProducerApplication.java
@@ -0,0 +1,14 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerApplication {
+
+	//https://github.com/SimpleProgramming/springboot-kafka
+	
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerApplication.class, args);
+	}
+}
